c58b3e5ba2f387f82e3d5fa31daee882fe260f92
wadii <wadiiprof@gmail.com>
2025-12-30 20:50:30 +0100

    first commit

---
+++ src/main/java/com/example/tp20/Tp20Application.java
@@ -0,0 +1,13 @@
+package com.example.tp20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp20Application.class, args);
+	}
+
+}
